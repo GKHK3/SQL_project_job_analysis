7fb0d398cae5a24cecf4267a4000a7f04076e39c
GKHK3 <garthk3@gmail.com>
2026-06-09 00:04:01 +0100

    upload image

--- project_sql/1_top_paying_jobs.sql
+++ project_sql/1_top_paying_jobs.sql
@@ -8,7 +8,6 @@ Top-paying data analyst jobs
 SELECT
     job_id,
     job_title,
-    -- job_location,
     job_schedule_type,
     salary_year_avg,
     job_posted_date,
@@ -19,6 +18,7 @@ LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
 
 WHERE job_title = 'Data Analyst' AND 
       job_location = 'Anywhere' AND
+      job_work_from_home = TRUE AND
       salary_year_avg IS NOT NULL
 ORDER BY
       salary_year_avg DESC
